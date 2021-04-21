b17a827c8f9c1677df3abb5ca1255a174204df47
Weiyan Guo <github@weiyanguo.cn>
2021-04-21 17:14:49 +0800

    now you can directly run the script

--- choose_anything.py
+++ choose_anything.py
@@ -29,7 +29,7 @@ def main():
 
     reg = re.compile(args.reg)
     file_list = [file for file in os.listdir(args.from_folder) if reg.search(file)]
-    if args.except_reg:
+    if args.ignore_reg:
         except_reg = re.compile(args.except_reg)
         file_list = [file for file in file_list if not except_reg.search(file)]
     print(f"There are {len(file_list)} items found in the from_folder")
