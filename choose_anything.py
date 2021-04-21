90890bc68f5eaef7102feb10b45cb15d752adce4
Weiyan Guo <github@weiyanguo.cn>
2021-04-21 16:36:15 +0800

    add the function to ignore some specific items

--- choose_anything.py
+++ choose_anything.py
@@ -22,12 +22,16 @@ def main():
     arg_parser.add_argument("to_folder", help="Copy the chosen data to here")
     arg_parser.add_argument("n", help="Choose how many items")
     arg_parser.add_argument("reg", help="The regex to find your items in the folder")
+    arg_parser.add_argument("--except_reg", help="The regex to match the item you don't want to choose")
 
     args = arg_parser.parse_args()
     args = args_check(args)
 
     reg = re.compile(args.reg)
     file_list = [file for file in os.listdir(args.from_folder) if reg.search(file)]
+    if args.except_reg:
+        except_reg = re.compile(args.except_reg)
+        file_list = [file for file in file_list if not except_reg.search(file)]
     print(f"There are {len(file_list)} items found in the from_folder")
     rng = np.random.default_rng()
     chosen_file_list = list(rng.choice(file_list, size=args.n, replace=False))
